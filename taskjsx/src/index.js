f7edaded090fe49d7760291710f7aa4ca254fa5a
Anassiddiqui-cedcoss <mohdanassiddiqui@cedcoss.com>
2022-08-19 15:35:04 +0530

    In the above task find all the cities whose name start with 'B'

--- taskjsx/src/index.js
+++ taskjsx/src/index.js
@@ -5,8 +5,8 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-var cities=["Lucknow","Agra","Kanpur","Gorakhpur","Rajasthan","Goa","Nepal"]
-var sortedcity=cities.sort()
+var cities=["Lucknow","Agra","Kanpur","Gorakhpur","Rajasthan","Goa","Nepal","Begusarai","Buxar","Bihar Sharif"]
+var sortedcity=cities.filter((item) => item.startsWith("B"))
 console.log(sortedcity)
 const element = (<ul>{sortedcity.map((item) => {return <li>{item}</li>})}</ul>);
   root.render(
